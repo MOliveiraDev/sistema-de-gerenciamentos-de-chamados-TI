7fd2e08baada393c3dea920a650eaeac87c977fa
João Maicon de Oliveira Monte <oliveiramaicon16637@gmail.com>
2026-04-14 13:40:44 -0300

    first commit

---
+++ src/main/java/ateneu/sgcti/SgctiApplication.java
@@ -0,0 +1,13 @@
+package ateneu.sgcti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgctiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgctiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ateneu/sgcti/SgctiApplicationTests.java
@@ -0,0 +1,13 @@
+package ateneu.sgcti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgctiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
