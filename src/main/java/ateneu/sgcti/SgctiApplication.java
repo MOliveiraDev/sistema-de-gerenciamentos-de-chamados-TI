d17531737cdf8bb8247e4a79ea65174d0429d75b
João Maicon de Oliveira Monte <oliveiramaicon16637@gmail.com>
2026-04-15 18:53:00 -0300

    feat: integrar suporte a variáveis de ambiente usando .env e ajustar configuração do banco

--- src/main/java/ateneu/sgcti/SgctiApplication.java
+++ src/main/java/ateneu/sgcti/SgctiApplication.java
@@ -1,5 +1,6 @@
 package ateneu.sgcti;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SgctiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(SgctiApplication.class, args);
 	}
 
