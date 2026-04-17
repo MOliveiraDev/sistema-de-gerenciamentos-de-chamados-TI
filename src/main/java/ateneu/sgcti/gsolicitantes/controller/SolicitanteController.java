85afe7ba9e8dcba7243cb86fd0582d84ace2a8c2
João Maicon de Oliveira Monte <oliveiramaicon16637@gmail.com>
2026-04-17 14:36:34 -0300

    feat: adicionar autorização de acesso para ADMIN no SolicitanteController

--- src/main/java/ateneu/sgcti/gsolicitantes/controller/SolicitanteController.java
+++ src/main/java/ateneu/sgcti/gsolicitantes/controller/SolicitanteController.java
@@ -6,6 +6,7 @@ import ateneu.sgcti.gsolicitantes.service.SolicitanteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/solicitantes/v1")
+@PreAuthorize("hasRole('ADMIN')")
 @RequiredArgsConstructor
 public class SolicitanteController {
 
